2fcf1331bb4b4fb1b0cb7b27f6ad6e7a81a937ba
Romain niam0r <niamor@mm.st>
2018-03-27 17:38:44 +0100

    let component;   beforeEach(() => {     component = renderComponent(CommentBox);   });

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class CommentBox extends React.Component {
   render() {
     return (
-      <div>
+      <div className="comment-box">
         <textarea />
         <button></button>
       </div>
