95eca4bc6dafe8f31ca171855c8edfc084070a64
Romain niam0r <niamor@mm.st>
2018-03-27 19:42:06 +0100

    implemented connect

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class CommentBox extends React.Component {
+class CommentBox extends React.Component {
   constructor(props) {
     super(props);
 
@@ -13,6 +15,8 @@ export default class CommentBox extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
+
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: '' });
   }
 
@@ -31,3 +35,6 @@ export default class CommentBox extends React.Component {
     );
   }
 }
+
+export default connect(null, actions)(CommentBox);
+// binding all actions, section 2 lecture 20
