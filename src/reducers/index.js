39e6c022c21ba988cef94d488523fd23febad07c
Romain niam0r <niamor@mm.st>
2018-03-29 16:27:30 +0100

    submitting comment working and all test passing

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import commentsReducer from './comments';
 
 const rootReducer = combineReducers({
-  comments: (state = []) => state
+  comments: commentsReducer
 });
 
 export default rootReducer;
