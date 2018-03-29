5ace53d2d96b2866730089a544b844cb5f7717b3
Romain niam0r <niamor@mm.st>
2018-03-29 12:54:30 +0100

    implemented handles action of type SAVE_COMMENT

--- test/reducers/comments_test.js
+++ test/reducers/comments_test.js
@@ -5,10 +5,11 @@ import { SAVE_COMMENT } from '../../src/actions/types';
 describe('Comments Reducer', () => {
   it('handles action with unknown type', () => {
     // expect(commentReducer()).to.be.instanceOf(Array);
-    expect(commentReducer()).to.eql([]);
+    expect(commentReducer(undefined, {})).to.eql([]);
   });
 
   it('handles action of type SAVE_COMMENT', () => {
-
+    const action = { type: SAVE_COMMENT, payload: 'new comment'};
+    expect(commentReducer([], action)).to.eql(['new comment']);
   });
 })
